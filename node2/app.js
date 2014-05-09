54c9a80ad93f6ae2aa9be546fe2b4fb5b3be0eb0
sarah <sarahgarcin@hotmail.com>
2014-05-09 14:24:59 +0200

    visualisateurs fonctionne avec 2 users + bug disconnect

---
+++ node2/app.js
@@ -0,0 +1,51 @@
+var express = require('express'); //inclusion du framework express
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server); // création d'un objet app avec express + serveur + socket.io
+
+server.listen(8333);
+io.set('log level', 1)
+
+app.use(express.static(__dirname + '/public'));
+
+
+var req = app.get('/', function (req, res) { // c'est ici qu'on indique les différentes routes (URLs)
+  //res.sendfile(__dirname + '/public/index.html');
+});
+
+var users = [];
+
+io.sockets.on('connection', function (socket, user) {
+
+
+    socket.on('nouveau_client', function (user) { 
+        socket.set('user', user);
+        users.push(user);
+        updateClients();
+    });
+
+    // socket.on('textarea', function (data){
+    //     socket.broadcast.emit('textarea', data);
+
+    // });
+
+    socket.on('message', function (data) {        
+        socket.broadcast.emit('message',data);
+
+      });
+
+    // socket.on('disconnect', function (user) {
+    //     for(var i=0; i<users.length; i++) {
+    //         if(users[i] == users.indexOf(user)) {
+    //             delete users[users[i]];
+    //         }
+    //     }
+    //     console.log(users.indexOf(user));
+    //     updateClients(); 
+    // });
+
+    function updateClients() {
+        io.sockets.emit('update', users);
+    }
+
+});
