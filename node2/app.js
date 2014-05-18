fd3b930bb96a0580de288e074ae0e1667991b67c
sarah <sarahgarcin@hotmail.com>
2014-05-18 22:43:30 +0200

    multi users (almost there)

--- node2/app.js
+++ node2/app.js
@@ -1,4 +1,5 @@
 var express = require('express'); //inclusion du framework express
+var mysql = require('mysql'); // inclusion de msql
 var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server); // création d'un objet app avec express + serveur + socket.io
@@ -7,6 +8,19 @@ server.listen(1337);
 io.set('log level', 1)
 
 app.use(express.static(__dirname + '/public'));
+
+// définition de la base de données 
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'node'
+})
+
+// Log any errors connected to the db
+db.connect(function(err){
+    if (err) console.log(err)
+})
 
 
 var req = app.get('/', function (req, res) { // c'est ici qu'on indique les différentes routes (URLs)
@@ -31,21 +45,22 @@ io.sockets.on('connection', function (socket) {
     });
 
     socket.on('notes', function (data) { 
-            socket.broadcast.emit('notes', data);
-        // for(var i=0; i<users.length; i++) {
-        // if(users[0]){      
-        //     socket.broadcast.emit('message un', data);
-        // }
-        // io.sockets.socket(users[1]).emit("message un", data);
-        // }
-    });
+        socket.on('id', function (id){
+            console.log(id);
+            if(id == users[2]){
+               socket.broadcast.emit('notes2', data);
+               console.log('detect 3');
+            }
 
-    // socket.on('notes', function (data) {        
-    //     io.sockets.socket(users[2]).emit("new message", data);
-    //     // if(users[1]){      
-    //     //     socket.broadcast.emit('new message', data);
-    //     // }
-    // });
+            else if(id == users[0] || id == users[1]) {
+                socket.broadcast.emit('notes', data);
+            }
+                 
+        })
+
+
+        // socket.broadcast.emit('notes', data);
+    });
 
     socket.on('user image', function (msg) {
       socket.broadcast.emit('user image', socket.user, msg);
