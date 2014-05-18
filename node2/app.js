dc054b0776395a6243aec010d2e6b261c422a864
sarah <sarahgarcin@hotmail.com>
2014-05-18 09:22:40 +0200

    correction bug socket.send + counting users

--- node2/app.js
+++ node2/app.js
@@ -3,7 +3,7 @@ var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server); // création d'un objet app avec express + serveur + socket.io
 
-server.listen(8333);
+server.listen(1337);
 io.set('log level', 1)
 
 app.use(express.static(__dirname + '/public'));
@@ -14,9 +14,14 @@ var req = app.get('/', function (req, res) { // c'est ici qu'on indique les diff
 });
 
 var users = [];
+var socketCount = 0
 
-io.sockets.on('connection', function (socket, user) {
+io.sockets.on('connection', function (socket) {
 
+    // Socket has connected, increase socket count
+    socketCount++
+    // Let all sockets know how many are connected
+    io.sockets.emit('users connected', socketCount)
 
     socket.on('nouveau_client', function (user) { 
         // socket.set('user', user);
@@ -25,9 +30,22 @@ io.sockets.on('connection', function (socket, user) {
         updateClients();
     });
 
-    socket.on('message', function (data) {        
-        socket.broadcast.emit('message',data);
-      });
+    socket.on('notes', function (data) { 
+            socket.broadcast.emit('notes', data);
+        // for(var i=0; i<users.length; i++) {
+        // if(users[0]){      
+        //     socket.broadcast.emit('message un', data);
+        // }
+        // io.sockets.socket(users[1]).emit("message un", data);
+        // }
+    });
+
+    // socket.on('notes', function (data) {        
+    //     io.sockets.socket(users[2]).emit("new message", data);
+    //     // if(users[1]){      
+    //     //     socket.broadcast.emit('new message', data);
+    //     // }
+    // });
 
     socket.on('user image', function (msg) {
       socket.broadcast.emit('user image', socket.user, msg);
@@ -39,7 +57,10 @@ io.sockets.on('connection', function (socket, user) {
                 users.splice(i, 1);
             }
         }
-        updateClients(); 
+        updateClients();
+        // Decrease the socket count on a disconnect, emit
+        socketCount--
+        io.sockets.emit('users connected', socketCount) 
     });
 
     function updateClients() {
--- node2/issues.md
+++ node2/issues.md
@@ -4,24 +4,45 @@
 
 ####BUGS
 
-*résolu* 
+*résolu 11/05/14* 
 - Bug sur les regex, faire en sorte qu'elle ne soit prise que si le mot est tout seul, et non dans un mot. 
 (exemple si j'écris "prendre", le pr se transforme en "pour" pour donne "pourendre")
 
-*résolu* 
+*résolu 12/05/14* 
 [Avec la méthode "users.splice(i, 1);"]
 - Quand un user ferme sa fenêtre, il ne se déconnecte pas vraiment et reste dans la liste des users connectés. 
 
-- Pour l'instant, dans le visualisateur, la ligne de temps n'apparait que lorsqu'on fait un keyup ou keydown.
-
-*résolu* 
+*résolu - 12/05/14* 
 [var textArea = $('#visu'); textArea.scrollTop(textArea[0].scrollHeight - textArea.height());]
 - Le pad "visualisateur" ne suit pas le texte. 
 
+- [12/05/14] Pour l'instant, dans le visualisateur, la "ligne de temps" (|) n'apparait que lorsqu'on fait un keyup ou keydown.
+
 ####TO DO
 
-- Mettre plusieurs visualisateurs (à ce jour, on ne voit qu'un seul user dans le visu)
+- Pad Perso 
+	- [13/05/14] Faire un WYSIWYG (avec les fonctionnalités établies: bold, souligné etc.)
+	- [13/05/14] Automatiser les Regex -> système qui permet de les ajouter
+
+- Visualisateurs
+
+	(1)- [12/05/14] Mettre plusieurs visualisateurs (à ce jour, on ne voit qu'un seul user dans le visu - 12/05/14)
+
 
 - Faire la rivière
 
-- Faire la timeline -> timecode
+	*fait - 13/05/14* 
+	- Insérer des images depuis un ordinateur
+
+	- [13/05/14] Insérer des images depuis internet
+
+	- [13/05/14] Insérer les url automatiquement depuis le pad. 
+
+
+(2)- Faire la timeline -> timecode
+	
+	- [13/05/14] Timecode sur les lignes
+
+	- [13/05/14] Timecode sur les images 
+
+	- [13/05/14] Lier les différents pad par les timecodes
